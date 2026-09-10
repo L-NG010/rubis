45c348908cbfb9aa0a48e6dd21aa567ab7ff4a36
Lang Natanegara <langnatanegara73@gmail.com>
2026-09-10 20:20:18 +0100

    asddf

--- main.py
+++ main.py
@@ -23,8 +23,8 @@ def main():
         from playwright.sync_api import sync_playwright
 
         # --- KONFIGURASI SUPABASE LOCAL ---
-            SUPABASE_URL = "https://bsunzewnefxyamapczzw.supabase.co"
-            SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJzdW56ZXduZWZ4eWFtYXBjenp3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODgxNjQ3MjQsImV4cCI6MjEwMzc0MDcyNH0.QzoTFIGOREcUXl6SfxvpxA61d53g0hTB9x8Dsbfthws"
+        SUPABASE_URL = "https://bsunzewnefxyamapczzw.supabase.co"
+        SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJzdW56ZXduZWZ4eWFtYXBjenp3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODgxNjQ3MjQsImV4cCI6MjEwMzc0MDcyNH0.QzoTFIGOREcUXl6SfxvpxA61d53g0hTB9x8Dsbfthws"
 
         def kill_browser_processes():
             """Menutup proses browser agar profil data tidak terkunci."""
